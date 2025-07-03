b77ac8500b66f0e11223d7953b583025707a442f
suren <suren@surendhiran>
2025-07-03 09:54:49 +0530

    commit3

--- TNS_Java_Program/src/com/tnsif/nonaccessmodifiers/FinalDemo.java
+++ TNS_Java_Program/src/com/tnsif/nonaccessmodifiers/FinalDemo.java
@@ -1,17 +1,19 @@
 package com.tnsif.nonaccessmodifiers;
 
-final class FinalDemo {
+//final 
+class FinalDemo {
 	
 	final int Max = 100;
 	
-	final void display() {
+	//final
+	void display() {
 		System.out.println("show the value of max: "+Max);
 	}
 
 	public static void main(String[] args) {
 		FinalDemo fd = new FinalDemo();
 		fd.display();
-		fd.Max = 200;
+		//fd.Max = 200;
 
 	}
 
